c2d1db51ee3c77afb7e2589a8e42b0e1a0dcc482
Jess <jess2410@hotmail.fr>
2023-01-24 14:46:51 +0100

    resizing services cards + favicon + about section

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -24,30 +24,50 @@ export default function About() {
       data-aos-duration='1000'
     >
       <div className={styles.contentBx}>
-        <h2 className={styles.about_h3}>À propos</h2>
-        <p className={styles.text}>
-          Fort de mon activité professionnelle de plus de 20 ans dans la
-          communication extérieure, j’ai créé StepAdvise spécialisée dans le
-          conseil à la gestion du mobilier urbain, le respect de la
-          réglementation locale de la publicité, la gestion de la TLPE et
-          l’audit publicitaire. En effet, mon expérience auprès de nombreuses
-          collectivités territoriales (Ile de France, Grand-Ouest et Sud de la
-          France) associée à mon expertise sur les sujets des appels d’offres de
-          mobiliers urbains, des RLPi ou de la TLPE seront des atouts
-          indéniables mis à votre service pour répondre à vos attentes.
-        </p>
-        <p className={styles.text}>
-          La compétence de StepAdvise sur le milieu de l’Outdoor et de ses
-          acteurs sera un avantage dans votre volonté de développer votre
-          notoriété via ce canal publicitaire. StepAdvise s’appuie sur un
-          réfèrent juridique spécialisé dans le droit des contrats publics et
-          dispose de solutions digitales adaptées à la gestion des
-          collectivités. C’est pourquoi, actif sur tout le territoire,
-          StepAdvise vous propose son savoir faire afin de faciliter votre
-          processus décisionnel.
-        </p>
+        {/* <h2 className={styles.about_h3}>À propos</h2> */}
+        <div className={styles.wrapper}>
+          <div className={styles.imgBx}>
+            <Image
+              src={"/assets/cédric.jpg"}
+              width={300}
+              height={400}
+              // layout='responsive'
+              alt='Cédric Nédéléc'
+            ></Image>
+            <p
+              style={{
+                textAlign: "center",
+                fontStyle: "italic",
+                letterSpacing: "0.5px",
+                fontSize: "1.15rem",
+              }}
+            >
+              Cédric Nédélec
+            </p>
+          </div>{" "}
+          <p className={styles.text}>
+            Fort de mon activité professionnelle de plus de 20 ans dans la
+            communication extérieure, j’ai créé StepAdvise spécialisée dans le
+            conseil à la gestion du mobilier urbain, le respect de la
+            réglementation locale de la publicité, la gestion de la TLPE et
+            l’audit publicitaire. En effet, mon expérience auprès de nombreuses
+            collectivités territoriales (Ile de France, Grand-Ouest et Sud de la
+            France) associée à mon expertise sur les sujets des appels d’offres
+            de mobiliers urbains, des RLPi ou de la TLPE seront des atouts
+            indéniables mis à votre service pour répondre à vos attentes.
+          </p>
+          <p className={styles.text}>
+            La compétence de StepAdvise sur le milieu de l’Outdoor et de ses
+            acteurs sera un avantage dans votre volonté de développer votre
+            notoriété via ce canal publicitaire. StepAdvise s’appuie sur un
+            réfèrent juridique spécialisé dans le droit des contrats publics et
+            dispose de solutions digitales adaptées à la gestion des
+            collectivités. C’est pourquoi, actif sur tout le territoire,
+            StepAdvise vous propose son savoir faire afin de faciliter votre
+            processus décisionnel.
+          </p>
+        </div>
       </div>
-      <div className={styles.imgBx}></div>
     </section>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel='icon' href='../public/favicon/favicon.jpg' />
+      </Head>
       <Component {...pageProps} />
       <ToastContainer />
     </>
