a914be398d3110cdbf1e0dff5bd6cc5a1f9c5782
Jess <jess2410@hotmail.fr>
2023-06-09 16:04:33 +0200

    correction build

--- components/Pe/Pe.jsx
+++ components/Pe/Pe.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-import styles from "./pe.module.css";
+import styles from "./Pe.module.css";
 
 function Pe() {
   return (
